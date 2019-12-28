81daf1b9ebc4d286299bfe29805200cf23dec9f6
國井弘一 <kuniiskywalker@gmail.com>
2019-12-28 20:56:00 +0900

    224*224サイズで生成するデータセットバッチ追加

---
+++ generate_dataset/app_224.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import os, glob
+import numpy as np
+from sklearn import model_selection
+
+classes = ["toda", "ushiku"]
+num_classes = len(classes)
+image_size = 224
+
+X = []
+Y = []
+
+for index, classlabel in enumerate(classes):
+	photo_dir = "/data/" + classlabel
+	files = glob.glob(photo_dir + "/*.jpg")
+	for i, file in enumerate(files):
+		print(file)
+		image = Image.open(file)
+		image = image.convert("RGB")
+		print(image)
+		image = image.resize((image_size, image_size))
+		data = np.asarray(image)
+		X.append(data)
+		Y.append(index)
+
+X = np.array(X)
+Y = np.array(Y)
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, Y)
+xy = (X_train, X_test, y_train, y_test)
+
+np.save("/data/imagefiles_224.npy", xy)
